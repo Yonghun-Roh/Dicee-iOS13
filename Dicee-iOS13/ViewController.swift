6bd9a16028d8e759ceb00eca7db1d25102599005
Ro YongHun <mogi3206@naver.com>
2022-08-31 22:17:33 +0900

    주사위 만들기. 오른쪽 주사위가 xcode에서는 하나이지만 실행하면 2로 바뀜

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -9,12 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var diceImageView1: UIImageView!
+    @IBOutlet var diceImageView2: UIImageView!
+    
+    
+    /* who.what = value
+    who : diceImageView11
+    what : image
+     diceImageView
+     
+     */
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    
+        diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
+        diceImageView1.alpha = 0.5
+        
+        diceImageView2.image = #imageLiteral(resourceName: "DiceTwo")
+    
+        
     }
-
 
 }
 
