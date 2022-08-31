ec74e26e8592264aad4fa71021b0a8db0234709d
Ro YongHun <mogi3206@naver.com>
2022-08-31 22:27:23 +0900

    주석 추가 who.what = value imageLiteral

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -14,20 +14,29 @@ class ViewController: UIViewController {
     
     
     /* who.what = value
-    who : diceImageView11
+    who : diceImageView1
     what : image
      diceImageView
+     
+     누가? 또는 뭐가? = diceImageView1이
+     뭔데? = 이미지다
+     값은? = 주사위 6을 대입할꺼다
      
      */
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-    
+ 
+        // who         what  = value
         diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
         diceImageView1.alpha = 0.5
         
         diceImageView2.image = #imageLiteral(resourceName: "DiceTwo")
+        // 이미지를 보고 선택할 수 있게 해주는 것은 imageLiteral 인데
+        // 현재 내 xcode에서는 에러가 발생함. 해결 방법은 #imageLiteral( 을 입력해주면 된다.
+        // diceImageView1.image = #imageLiteral(
+        
     
         
     }
