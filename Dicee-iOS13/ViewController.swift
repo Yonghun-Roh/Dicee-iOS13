8f66b9ef01d34b86c6901d673975d2859d7337c2
Ro YongHun <mogi3206@naver.com>
2022-09-05 22:04:27 +0900

    안젤라 스터디 주사위 최종

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -7,43 +7,32 @@
 //
 
 import UIKit
+//코코아프레임워크 UIKit에서 가져온다.
 
 class ViewController: UIViewController {
     @IBOutlet var diceImageView1: UIImageView!
     @IBOutlet var diceImageView2: UIImageView!
     
-    
-    /* who.what = value
-    who : diceImageView1
-    what : image
-     diceImageView
      
-     누가? 또는 뭐가? = diceImageView1이
-     뭔데? = 이미지다
-     값은? = 주사위 6을 대입할꺼다
-     
-     */
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
- 
-        // who         what  = value
-        diceImageView1.image = #imageLiteral(resourceName: "DiceSix")           
-        diceImageView2.image = #imageLiteral(resourceName: "DiceTwo")
-        // 이미지를 보고 선택할 수 있게 해주는 것은 imageLiteral 인데
-        // 현재 내 xcode에서는 에러가 발생함. 해결 방법은 #imageLiteral( 을 입력해주면 된다.
-        // diceImageView1.image = #imageLiteral(
-        
-    
-        
-    }
-
     @IBAction func rollButtonPressed(_ sender: UIButton) {
-        diceImageView1.image = #imageLiteral(resourceName: "DiceFour")
-        diceImageView2.image = #imageLiteral(resourceName: "DiceFour")
-    
+          
+        let diceArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+/*
+        // 앞으로 변경될 일이 없는 코드이기에 컴파일러에서 var을 let으로 변경시켜주라는 의견을 제시하여 let으로 변경.
+        // diceArray를 배열로 선언.
+      
+//        let diceArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+  */
         
+        diceImageView1.image = diceArray.randomElement()
+        //diceImageView1은 이미지이며 diceImage1에는 위에 선언된 diceArray 배열을 적용할 것이다. 무작위로.
+        diceImageView2.image = diceArray [Int.random(in: 0...5)]
+        // diceImageView2는 이미지이며 diceImage2에는 위에 선언된 diceArray 배열을 적용할 것이다. 무작위로 적용하되 정수에서 0에서 5까지 배열 중에서.
+        
+       
+      
+    
+       
     }
 }
 
